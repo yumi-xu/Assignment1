6a7f4eb6ce59a4c3c2d74d8a8be791558d057d6b
yumi-xu <happyleaf90@outlook.com>
2024-09-25 19:37:34 -0700

    revert initial inputs

--- context/GameContext.js
+++ context/GameContext.js
@@ -5,15 +5,15 @@ export const GameContext = createContext();
 
 // Create a provider component
 export const GameProvider = ({ children }) => {
-  const [currentScreen, setCurrentScreen] = useState('start'); // start/game
-  const [name, setName] = useState("aaa");
-  const [email, setEmail] = useState("123456@qq.com");
-  const [phone, setPhone] = useState("1234567895");
-
   // const [currentScreen, setCurrentScreen] = useState('start'); // start/game
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [phone, setPhone] = useState("");
+  // const [name, setName] = useState("aaa");
+  // const [email, setEmail] = useState("123456@qq.com");
+  // const [phone, setPhone] = useState("1234567895");
+
+  const [currentScreen, setCurrentScreen] = useState('start'); // start/game
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
   return (
     <GameContext.Provider
